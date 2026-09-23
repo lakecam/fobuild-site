872c67c90e483e63980e31a6496b0e391960bffb
Lake Cam <thelakecam@gmail.com>
2026-09-22 23:00:53 -0700

    Add Mobile App mode (Solstice Home, Field, Harvest Commons); declutter the control panel; remove the site map and journey player

---
+++ assets/battery-DXgJPyEr.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chevron-down-B0OCafTR.js";var t={name:`battery`,size:24,node:[[`path`,{d:`M 22 14 L 22 10`,key:`nqc4tb`}],[`rect`,{x:`2`,y:`6`,width:`16`,height:`12`,rx:`2`,key:`13zb55`}]]};t.node;var n=e(t);export{n as t};
--- assets/plus-g-st0P1o.js
+++
@@ -1,1 +0,0 @@
-import{I as e}from"./index-9uCxEEwS.js";var t={name:`chevron-right`,size:24,node:[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]};t.node;var n=e(t),r={name:`plus`,size:24,node:[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`M12 5v14`,key:`s699le`}]]};r.node;var i=e(r);export{n,i as t};
